e6db29974e74eb27244eed7ebc5ce09ecf894da2
Pedro H. Alves <leonkene18@gmail.com>
2023-10-30 10:56:54 -0300

    aaa

---
+++ React_EventPlus/src/routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import HomePage from "./pages/Home/HomePage";
+import LoginPage from "./pages/Login/LoginPage";
+
+const Rotas = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route />
+            </Routes>
+        </BrowserRouter>
+    )
+}
