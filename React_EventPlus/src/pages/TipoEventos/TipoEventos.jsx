e378f081e7df5562c3f294cc848b9f6b618d1ae0
Pedro H. Alves <leonkene18@gmail.com>
2023-10-31 08:37:42 -0300

    Rotas definidas e pages basicas criadas

---
+++ React_EventPlus/src/pages/TipoEventos/TipoEventos.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TipoEventos = () => {
+    return (
+        <div>
+            <h1>Tipo Eventos</h1>
+        </div>
+    );
+};
+
+export default TipoEventos;
--- React_EventPlus/src/routes.js
+++ React_EventPlus/src/routes.js
@@ -1,15 +1,22 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
-import HomePage from "./pages/Home/HomePage";
+import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/Login/LoginPage";
+import EventosPage from "./pages/EventosPage/EventosPage";
+import TipoEventos from "./pages/TipoEventos/TipoEventos";
 
 const Rotas = () => {
     return(
         <BrowserRouter>
             <Routes>
-                <Route />
+                <Route element={ <HomePage />} path={"/"}  exact/>
+                <Route element={ <LoginPage />} path={"/login"} exact/>
+                <Route element={ <EventosPage />} path={"/eventos"} exact/>
+                <Route element={ <TipoEventos />} path={"/tipoEventos"} exact/>
             </Routes>
         </BrowserRouter>
     )
 }
+
+export default Rotas;
