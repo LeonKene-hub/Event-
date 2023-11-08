3a87d8f2441c9f4ae7b8f3366ee77b65e6d76edc
Pedro H. Alves <leonkene18@gmail.com>
2023-11-08 10:52:17 -0300

    axios instalado

--- React_EventPlus/src/pages/HomePage/HomePage.jsx
+++ React_EventPlus/src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import Banner from '../../components/Banner/Banner'
-import MainContent from '../../components/Main/MainContent'
-import VisionSection from '../../components/VisionSection/VisionSection';
+import React from "react";
+import './HomePage.css'
+
+import Banner from "../../components/Banner/Banner";
+import MainContent from "../../components/Main/MainContent";
+import VisionSection from "../../components/VisionSection/VisionSection";
+import ContactSection from "../../components/ContactSection/ContactSection";
+import Title from "../../components/Title/Title";
+import NextEvent from "../../components/NextEvent/NextEvent";
+import Container from "../../components/Container/Container";
 
 const Home = () => {
-    return (
-        <MainContent>
-            <Banner />
-            <VisionSection />
-        </MainContent>
-    );
+  return (
+    <MainContent>
+      <Banner />
+      <section className="proximos-eventos">
+        <Container>
+          <Title titleText={"Proximos Eventos"} />
+
+          <div className="events-box">
+            <NextEvent />
+            <NextEvent />
+            <NextEvent />
+            <NextEvent />
+          </div>
+        </Container>
+      </section>
+      <VisionSection />
+      <ContactSection />
+    </MainContent>
+  );
 };
 
 export default Home;
